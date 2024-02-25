63ba3da3aeec5e51af9d2c42c3fa9ab7d3a9792d
Tammy Hartline <93747265+tjhartline@users.noreply.github.com>
2024-02-24 23:25:01 -0600

    Update animal_shelter.py

--- animal_shelter.py
+++ animal_shelter.py
@@ -32,22 +32,38 @@
 |  causing it not to launch on the server when running the main app.py file.]                               |
 |  ### - End                                                                                                |
 |*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*|
+|  ###  Start --02/2024 - TH                                                                                |
+|  [Finally removed unneeded methods and cleaned up code file to launch MVP.]                               |
+|  ### - End                                                                                                |
+|*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*|
 '''
+# Import required libraries
 import sqlite3
+import pandas as pd
+import os
 
+# Create animal shelter class
 class AnimalShelter(object):
-    def __init__(self, db_path=':memory:', username=None, password=None):
+    def __init__(self, db_path=':memory:'):
         self.db_path = db_path
-        self.username = username
-        self.password = password
         self._create_table()
 
+
+    # create table to store data using SQLite 
     def _create_table(self):
+        # if the database is in memory, there is no need to create a table
+        if self.db_path == ':memory:':
+            db_file = "animals.db"
+            # if the database file exists, there is no need to create a table
+            if os.path.exists(db_file):
+                return
+
+        # checking if table exists and if not, create it with all the columns located in the csv file    
         query_str = '''
             CREATE TABLE IF NOT EXISTS animals (
+                animal_id INTEGER PRIMARY KEY,
                 age_upon_outcome TEXT,
-                animal_id PRIMARY KEY,
-                animal_type,
+                animal_type TEXT,
                 breed TEXT,
                 color TEXT,
                 date_of_birth TEXT,
@@ -59,132 +75,36 @@ class AnimalShelter(object):
                 sex_upon_outcome TEXT,
                 location_lat REAL,
                 location_long REAL,
-                age_upon_outcome_in_weeks REAL,
-                rescue_type TEXT
+                age_upon_outcome_in_weeks REAL
             )
         '''
+        # connect to sqlite3, create table and read in the csv file
         with sqlite3.connect(self.db_path) as conn:
             cursor = conn.cursor()
             cursor.execute(query_str)
+            df = pd.read_csv("./aac_shelter_outcomes.csv")
+            df.to_sql('animals', conn, if_exists='replace', index=False)
+                
 
-        
-    def unq_animal_types(self):
-        query_str = 'SELECT DISTINCT animal_type FROM animals'
-
-        with sqlite3.connect(self.db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(query_str)
-            result = cursor.fetchall()
-
-        return [row[0] for row in result]
-
-    def createOne(self, data):
-        '''
-        Implement the C in CRUD.
-        Insert document into the specified SQLite table.
-        '''
-        if data:
-            # Extract breed from the data
-            breed = data.get('breed', 'N/A')
-
-            # Categorize rescue type based on breed
-            if breed in ['Labrador Retriever Mix', 'Chesapeake Bay Retriever', 'Newfoundland']:
-                data['rescue_type'] = 'water'
-            elif breed in ['German Shepard', 'Alaskan Malamute', 'Old English Sheepdog', 'Siberian Husky', 'Rottweiler']:
-                data['rescue_type'] = 'mountain'
-            elif breed in ['Doberman Pinscher', 'German Shepard', 'Golden Retriever', 'Bloodhound', 'Rottweiler']:
-                data['rescue_type'] = 'disaster'
-            else:
-                data['rescue_type'] = 'unknown'  # Handle other breeds
-
-            # Insert the data into the SQLite table
-            with sqlite3.connect(self.db_path) as conn:
-                cursor = conn.cursor()
-                placeholders = ', '.join(['?' for _ in data])
-                columns = ', '.join(data.keys())
-                values = tuple(data.values())
-                query_str = f'INSERT INTO animals ({columns}) VALUES ({placeholders})'
-                cursor.execute(query_str, values)
-                conn.commit()
-
-            return True
-        else:
-            print('\nNothing to save, data parameter is empty.')
-            return False
-
-    def read(self, projection=None, query=None, username=None, password=None):
-        """
-        Reads records from the database.
-
-        Parameters:
-        - projection (list or None): A list specifying the columns to include.
-        - query (tuple or None): A tuple specifying the query conditions.
-    
-        Returns:
-        - list: A list of records.
-        """
-        query_str = 'SELECT *' if projection is None else f"SELECT {', '.join(projection)}"
-        query_str += ' FROM animals'
+    # Create a read method for querying the database
+    def read(self, query=None):
+        query_str = 'SELECT * FROM animals'
         params = None
 
+        # if there is a query, add it to the query string
         if query is not None:
             query_str += f" WHERE {query[0]}"
             params = query[1]
 
+        # connect to sqlite3 and read in the query
         with sqlite3.connect(self.db_path) as conn:
             cursor = conn.cursor()
 
+            # if there are parameters, use them in the query
             if params is not None:
-                cursor.execute(query_str, params)
+                df = pd.read_sql_query(query_str, conn, params=params)
+
+            # if there are no parameters, just run the query
             else:
-                cursor.execute(query_str)
-
-            result = cursor.fetchall()
-
-        columns = [
-            'age_upon_outcome', 'animal_id', 'animal_type', 'breed', 'color',
-            'date_of_birth', 'datetime', 'monthyear', 'name', 'outcome_subtype',
-            'outcome_type', 'sex_upon_outcome', 'location_lat', 'location_long',
-            'age_upon_outcome_in_weeks', 'rescue_type'
-        ]
-
-        records = [dict(zip(columns, row)) for row in result]
-        return records
-
-
-    def update(self, query, update_data, multi=False):
-        '''
-        Implement the U in CRUD
-        Update option
-        '''
-        if query:
-            with sqlite3.connect(self.db_path) as conn:
-                cursor = conn.cursor()
-                set_clause = ', '.join([f"{key} = ?" for key in update_data.keys()])
-                query_str = f'UPDATE animals SET {set_clause} WHERE {query[0]}'
-                params = tuple(update_data.values()) + query[1] if multi else (tuple(update_data.values()), *query[1])
-                result = cursor.execute(query_str, params)
-                conn.commit()
-                print('\n\n')
-                return result.rowcount
-        else:
-            print('\nQuery parameter is empty')
-            return 0
-
-    def delete(self, query, multi=False):
-        '''
-        Implement the D in CRUD.
-        Options should be to delete one or delete many.
-        '''
-        if query:
-            with sqlite3.connect(self.db_path) as conn:
-                cursor = conn.cursor()
-                query_str = f'DELETE FROM animals WHERE {query[0]}'
-                params = query[1] if multi else (query[1],)
-                result = cursor.execute(query_str, params)
-                conn.commit()
-                print('\n\n')
-                return result.rowcount
-        else:
-            print('\nQuery parameter is empty')
-            return 0
+                df = pd.read_sql_query(query_str, conn)
+            return df
